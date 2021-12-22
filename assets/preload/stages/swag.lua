8403f6b999481da2a044312c6edc4332db222fc5
Noobandguestgang <95111164+Noobandguestgang@users.noreply.github.com>
2021-12-22 14:35:03 +0700

    Add files via upload

---
+++ assets/preload/stages/swag.lua
@@ -0,0 +1,10 @@
+function onCreate()
+
+  makeLuaSprite('swag','pissing_too',120,-35)
+  scaleObject('swag',0.521814815,0.521814815)
+  addLuaSprite('swag',false)
+
+
+
+	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
+end
