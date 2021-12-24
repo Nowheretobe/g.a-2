0cc37df3c375020add8e78bb48443fb5cbdb48b3
Noobandguestgang <95111164+Noobandguestgang@users.noreply.github.com>
2021-12-24 15:33:03 +0700

    Update redtunnel.lua

--- assets/preload/stages/redtunnel.lua
+++ assets/preload/stages/redtunnel.lua
@@ -1,9 +1,9 @@
 function onCreate()
 	-- background shit
-	makeLuaSprite('redtunnel', 'redtunnel', -1500, -900);
-	setLuaSpriteScrollFactor('redtunnel', 0, 0);
-	scaleObject('redtunnel', 2, 2)
-	addLuaSprite('redtunnel', false);
+	makeLuaSprite('redTunnel', 'redTunnel', -1500, -900);
+	setLuaSpriteScrollFactor('redTunnel', 0, 0);
+	scaleObject('redTunnel', 2, 2)
+	addLuaSprite('redTunnel', false);
 
 	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
 end
