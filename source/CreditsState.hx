fd49ed56e7e5961303ae23481d8749a872b669df
Noobandguestgang <95111164+Noobandguestgang@users.noreply.github.com>
2021-12-24 14:52:12 +0700

    Add files via upload

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -46,8 +46,9 @@ class CreditsState extends MusicBeatState
 		add(grpOptions);
 
 		var pisspoop = [ //Name - Icon name - Description - Link - BG Color
-			['Psych Engine Android Port'],
+			['Android Port'],
 			['M.A. Jigsaw',		    'majigsaw',		    'Main Coder of The Port',	 'https://www.youtube.com/channel/UC2Sk7vtPzOvbVzdVTWrribQ',	'0xFFC30085'],
+			['Noob and Guest gang',		    'nooband',		    'Porter',	 'https://www.youtube.com/channel/UCQBqqTBnOyLf35UoZUd0R0Q',	'0xFFC30085'],
 			['Psych Engine Team'],
 			['Shadow Mario',		'shadowmario',		'Main Programmer of Psych Engine',					'https://twitter.com/Shadow_Mario_',	'0xFFFFDD33'],
 			['RiverOaken',			'riveroaken',		'Main Artist/Animator of Psych Engine',				'https://twitter.com/river_oaken',		'0xFFC30085'],
